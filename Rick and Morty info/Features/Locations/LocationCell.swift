3bf7355aff4ba391938feb36d3abcb828aa7d819
michalgeci <michal.geci.1995@gmail.com>
2019-10-18 23:51:01 +0200

    Changed location ViewController

---
+++ Rick and Morty info/Features/Locations/LocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCell.swift
+//  Rick and Morty info
+//
+//  Created by Michal Geci on 18/10/2019.
+//  Copyright © 2019 MG. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
